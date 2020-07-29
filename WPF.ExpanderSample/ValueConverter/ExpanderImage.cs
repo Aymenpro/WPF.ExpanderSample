43b057d9fda4ee5304f9c0534891a9489b75751e
Aymenpro <49863420+Aymenpro@users.noreply.github.com>
2020-07-29 17:35:41 +0300

    Expander Sample

---
+++ WPF.ExpanderSample/ValueConverter/ExpanderImage.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace WPF.ExpanderSample.ValueConverter
+{
+    public class ExpanderImage : IValueConverter
+    {
+        // convert from Tag text to BitmapImage
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                return new BitmapImage(new Uri($"pack://application:,,,/{value.ToString()}"));
+            }
+            catch 
+            {
+                return null;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
